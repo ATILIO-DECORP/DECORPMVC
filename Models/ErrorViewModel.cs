ef0ad6ca2678aba5053e6da14334c3df9db1fe79
Atilio Dezan <atiliodezan.vix@gmail.com>
2025-03-28 16:23:11 -0300

    criado o projeto DECORPMVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DECORPMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
