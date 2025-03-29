d6f6a6f14abc094a11565eb67cdd8389fe8df392
Atilio Dezan <atiliodezan.vix@gmail.com>
2025-03-29 09:07:52 -0300

    CRUD DEPARTMENTS

---
+++ Data/DECORPMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DECORPMVC.Models;
+
+namespace DECORPMVC.Data
+{
+    public class DECORPMVCContext : DbContext
+    {
+        public DECORPMVCContext (DbContextOptions<DECORPMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DECORPMVC.Models.Departament> Departament { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using DECORPMVC.Data;
 
 namespace DECORPMVC
 {
@@ -33,6 +35,9 @@ namespace DECORPMVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<DECORPMVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("DECORPMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
